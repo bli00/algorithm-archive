fb558d2cc17dc69be0c8d35a8042addab924e8a2
bli940505 <ben@xage.com>
2020-01-16 15:56:39 -0800

    n queens updated

--- dynamic_programming/backtracking/n_queens/README.md
+++ dynamic_programming/backtracking/n_queens/README.md
@@ -1,3 +1,74 @@
 # N-Queens
 
+[Leetcode Problem 51](https://leetcode.com/problems/n-queens/)
+
+## Problem Statement
+
+The n-queens puzzle is the problem of placing n queens on an `n×n` chessboard such that no two queens attack each other.
+
+Given an integer `n`, return **all** distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens'
+placement, where 'Q' and '.' both indicate a queen and an empty space
+respectively.
+
+### Example
+
+```text
+Input: 4
+Output: [
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+Explanation: There exist two distinct solutions to the 4-queens
+```
+
+## Solution
+
+First, observe that each row must have exactly one queen. Similarly, each column
+must have exactly one queen.
+
+We can regard every diagonal as a function of `y = mx + b`, where `m` is the
+slope and `b` is the y-intercept. Since the diagonals will only have a slope of
+either `1` or `-1`. we have the functions `y = x + b` and `y = -x + b`, which
+implies `y + x = b` and `y - x = b`. A coordinate `(x',y')` on the same diagonal
+will have the same y-intercept as `(x,y)`. Solving for `b` gives us `x + y = x'
++ y'` and `y - x = y' - x'`. This is critial information that will be used to
+infer legal queen placements.
+
+Algorithm wise, since every row has to have exactly one queen. We iterate
+through the rows to find a column that's not under attack. Attempt to place a
+queen on this (row, column) combination, and let the recursion take care of
+itself. We can do this using the information described in the previous
+paragraphs.
+
+More specifically, we keep a list `queens` of column numbers. The indices of
+`queens` will correspond to the row of that placement whereas the element will
+correspond to the column. We also keep a list of `x + y` and `x - y` values to
+indicate the squares that are under attack, call these `sums` and `difs`. When
+there is a coordinate `(x', y')` we'd like to place a queen on, we check that
+`x'` is not in `queens`, `x' + y'` is not in `sums`, and `x' - y'` is not in
+`difs`.
+
+We use a backtracking method to save space. That is, we first prepare for the
+next recursion by updating `queens`, `sums`, and `difs`. After the current queen
+placement, we backtrack and remove the updates to `queens`, `sums`, and `difs`.
+Since the updates were inorder, the lists can be treated as a stack.
+
+Finally, the base case is when we have finished traversing all `n` rows. This
+can be inferred from the size of `queens`.
+
+### Time Complexity
+
+TODO
+
+### Space Complexity
+
 TODO
--- dynamic_programming/backtracking/n_queens/n_queens.py
+++ dynamic_programming/backtracking/n_queens/n_queens.py
@@ -1,120 +1,38 @@
-# Problem Statement:
-#
-# The n-queens puzzle is the problem of placing n queens on an n×n chessboard
-# such that no two queens attack each other.
-#
-# Given an integer n, return all distinct solutions to the n-queens puzzle.
-#
-# Each solution contains a distinct board configuration of the n-queens'
-# placement, where 'Q' and '.' both indicate a queen and an empty space
-# respectively.
-#
-# Example:
-#
-# ```
-# Input: 4
-# Output: [
-#  [".Q..",  // Solution 1
-#   "...Q",
-#   "Q...",
-#   "..Q."],
-#
-#  ["..Q.",  // Solution 2
-#   "Q...",
-#   "...Q",
-#   ".Q.."]
-# ]
-# ```
-#
-# Explanation: There exist two distinct solutions to the 4-queens puzzle as
-# shown above.
-
 from typing import List
 
-res = []
-curr = [] # location of queens
-occupied = {} # squares that's illegal
-traversed = set()
-N = 0
-
-# adds a solution on res
-def addToAnswer():
-    ans = [None] * N
-
-    for q in curr:
-        (i, j) = q
-        row = ['Q' if k == j else '.' for k in range(N)]
-        ans[i] = ''.join(row)
-
-    res.append(ans)
-
-def solveNQueensHelp(n: int):
-    # print(res, curr, occupied, N)
-    global curr, occupied
-    if n == 0:
-        addToAnswer()
-        return
-
-    directions = {(1,0), (-1,0), (0,-1), (0,1), (-1,-1), (1,1), (-1,1), (1,-1)}
-    for i in range(N):
-        for j in range(N):
-            cell = (i,j)
-            if occupied[cell] == 0:
-                # try to place a queen here
-                if frozenset(curr + [cell]) not in traversed:
-                    curr.append(cell)
-                    backtracker = []
-
-                    # mark illegal locations
-                    for (a,b) in directions:
-                        currCell = cell
-                        while currCell[0] >= 0 and currCell[0] < N and currCell[1] >= 0 and currCell[1] < N:
-                            # print(occupied)
-                            occupied[currCell] += 1
-                            backtracker.append(currCell)
-                            currCell = (currCell[0]+a,currCell[1]+b)
-
-                    solveNQueensHelp(n-1)
-
-                    # backtrack
-                    traversed.add(frozenset(curr))
-                    curr.pop()
-                    for currCell in backtracker:
-                        occupied[currCell] -= 1
-                
 def solveNQueens(n: int) -> List[List[str]]:
-    if n < 1:
-        return []
-    global N
-    N = n
-    for i in range(N):
-        for j in range(N):
-            occupied[(i,j)] = 0
+    # legal placements to be converted
+    placements = []
 
-    solveNQueensHelp(n)
-    return res
+    # iterate through each row and find viable placements
+    # the current row can be inferred from 'queens'
+    # queens[i] -> j: index is the row, element is the column
+    # sums contains i + j values
+    # difs contains i - j values
+    def queenSolver(queens: List[int], sums: List[int], difs: List[int]):
+        i = len(col) # infer current row
+        if i == n:
+            # base case
+            placements.append(queens.copy())
+            return
+        
+        for j in range(n):
+            # find all viable columns for placements
+            if j not in queens and i+j not in sums and i-j not in difs:
+                # backtracking method to save space
+                queens.append(j)
+                sums.append(i+j)
+                difs.append(i-j)
 
-res = []
-curr = [] # location of queens
-occupied = {} # squares that's illegal
-traversed = set()
-N = 0
-print(solveNQueens(2))
-res = []
-curr = [] # location of queens
-occupied = {} # squares that's illegal
-traversed = set()
-N = 0
-print(solveNQueens(3))
-res = []
-curr = [] # location of queens
-occupied = {} # squares that's illegal
-traversed = set()
-N = 0
-print(solveNQueens(4))
-res = []
-curr = [] # location of queens
-occupied = {} # squares that's illegal
-traversed = set()
-N = 0
-print(solveNQueens(5))
+                # recurse on possible values
+                queenSolver(queens, sums, difs)
+
+                # backtrack to prepare for next iteration
+                queens.pop()
+                sums.pop()
+                difs.pop()
+
+    dfs([],[],[])
+
+    # construct result
+    return [[('.'*j + 'Q' + '.'*(n-j-1)) for j in p] for p in placements]
