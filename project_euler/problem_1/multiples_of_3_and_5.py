2e1fc8797b485865bea3c03790cfb607fa5b2b66
bli940505 <ben@xage.com>
2020-02-03 17:42:55 -0800

    fixed technicality

--- project_euler/problem_1/multiples_of_3_and_5.py
+++ project_euler/problem_1/multiples_of_3_and_5.py
@@ -3,5 +3,5 @@ def multiples_of_3_and_5(n: int) -> int:
     def arith_series(k: int) -> int:
         return (k*(k+1))//2
 
-    return 3*arith_series(n//3) + 5*arith_series(n//5) - 15*arith_series(n//15)
+    return 3*arith_series((n-1)//3) + 5*arith_series((n-1)//5) - 15*arith_series((n-1)//15)
 
